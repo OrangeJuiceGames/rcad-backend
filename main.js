7cd020fde32df952ee8f9aa0d0b1b050d8f628db
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2022-11-29 15:33:52 -0800

    add cors

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/user')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 app.use(cors({
     origin: '*'
 }));
+
 app.use(express.json())
 app.use('/', routes)
 app.route('/')
