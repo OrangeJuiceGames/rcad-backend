6beb6b88d8eebd462d0292ce747bfa842ed4f5e2
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2022-11-29 15:17:52 -0800

    cors *

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const app = express()
 const routes = require('./routes/user')
 const mongoose = require('mongoose')
 
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json())
 app.use('/', routes)
 app.route('/')
